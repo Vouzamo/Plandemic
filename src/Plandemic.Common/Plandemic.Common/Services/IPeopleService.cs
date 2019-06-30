fadd6d504e0d19b50020f8b04e6366cafcf25303
John Askew <john.askew@deptagency.com>
2019-06-30 15:25:40 -0400

    Scaffold some CRUD style operations as an IService

---
+++ src/Plandemic.Common/Plandemic.Common/Services/IPeopleService.cs
@@ -0,0 +1,18 @@
+﻿using Plandemic.Common.Models;
+using System.Threading.Tasks;
+
+namespace Plandemic.Common.Services
+{
+    public interface IBaseService
+    {
+        Task<bool> TryGetByIdAsync<T, TId>(TId id, out T source) where T : IIdentifiable<TId>;
+        Task<bool> TryPutAsync<T, TId>(T source) where T : IIdentifiable<TId>;
+        Task<bool> TryPostAsync<T, TId>(T source, out TId id) where T : IIdentifiable<TId>;
+        Task<bool> TryDelete<T, TId>(TId id, bool scrict = false) where T : IIdentifiable<TId>;
+    }
+
+    public interface IPeopleService : IBaseService
+    {
+        
+    }
+}
