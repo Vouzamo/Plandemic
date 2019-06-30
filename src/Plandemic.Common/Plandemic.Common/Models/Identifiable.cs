a2b8e254c41bda4186c48d14cdf37ab562f5716a
John Askew <john.askew@deptagency.com>
2019-06-30 13:40:31 -0400

    Initial commit to add Common project.

---
+++ src/Plandemic.Common/Plandemic.Common/Models/Identifiable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plandemic.Common.Models
+{
+    public abstract class Identifiable
+    {
+        public Guid Id { get; set; }
+
+        public Identifiable()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Identifiable(Guid id) : this()
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Plandemic.Common/Plandemic.Common/Models/Multitenancy/Tenant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Plandemic.Common.Models.Multitenancy
+{
+    public class Tenant : Identifiable
+    {
+        public string Slug { get; set; }
+        public string Name { get; set; }
+
+        public Tenant() : base()
+        {
+            
+        }
+
+        public Tenant(string slug, string name) : this()
+        {
+            Slug = slug;
+            Name = name;
+        }
+    }
+}
